b940b088c5dcf172ccb2dd2ca88f7b8efcd22b10
WushanCloud <wushanyanyu@126.com>
2019-11-16 23:39:42 +0800

    蓝桥

---
+++ lanqiao3/test.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int Func(char num)
+{
+	int sum = 0;
+	while (num)
+	{
+		int tmp = 0;
+		tmp = num % 10;
+		if (tmp >= '1' && tmp<='9')
+		{
+
+		}
+	}
+}
+//十六进制转换十进制
+int main()
+{
+
+	printf("%d",Func(nums));
+	return 0;
+}
