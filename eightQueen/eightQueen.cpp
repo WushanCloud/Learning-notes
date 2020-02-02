637ad1c88f150526552579411246837f8f060b98
WushanCloud <wushanyanyu@126.com>
2020-02-02 23:30:28 +0800

    八皇后问题递归

--- eightQueen/eightQueen.cpp
+++ eightQueen/eightQueen.cpp
@@ -9,37 +9,24 @@ int sum = 0;
 
 int isTrue(int row, int col, int(*arr)[N])
 {
-<<<<<<< HEAD
+	//判断行
+	/*for (size_t i = 0; i < N; i++)
+	{
+		if (i == col)continue;
+		else
+		{
+			if (arr[row][i] == 1)
+			{
+				return 0;
+			}
+		}
+	}*/
 	//判断列
 	for (size_t i = 0; i < N; i++)
 	{
 		if (arr[i][col] == 1)
 		{
 			return 0;
-=======
-	////判断行
-	//for (size_t i = 0; i < N; i++)
-	//{
-	//	if (i == col)continue;
-	//	else
-	//	{
-	//		if (arr[row][i] == 1)
-	//		{
-	//			return 0;
-	//		}
-	//	}
-	//}
-	//判断列
-	for (size_t i = 0; i < N; i++)
-	{
-		if (i == col)continue;
-		else
-		{
-			if (arr[i][col] == 1)
-			{
-				return 0;
-			}
->>>>>>> cb147a9b4e81a0102309c628e6bd0e06d8d6bd20
 		}
 	}
 
@@ -48,7 +35,6 @@ int isTrue(int row, int col, int(*arr)[N])
 	//判断正对角
 	while (m != N && n != N)
 	{
-<<<<<<< HEAD
 		if (arr[m][n] == 1)
 		{
 			return 0;
@@ -56,56 +42,23 @@ int isTrue(int row, int col, int(*arr)[N])
 		m++, n++;
 	}
 
-	int k = N-1-col;
+	int k = N - 1 - col;
+	min = __min(min, k);
 	m = row - k; n = col + k;
+	//cout <<row<<" "<<col<<" "<< m << " " << n << endl;
 	//判断反对角
-	while (m !=N && n>0 )
+	while (m != N && n > 0)
 	{
 		if (arr[m][n] == 1)
 		{
 			return 0;
 		}
 		m++, n--;
-=======
-		if (m == row && n == col)
-		{
-			m++, n++;
-			continue;
-		}
-		else
-		{
-			if (arr[m][n] == 1)
-			{
-				return 0;
-			}
-			m++, n++;
-		}
-	}
-	int k = N-1-col;
-	m = row - k; n = col + k;
-	//判断反对角
-	while (m != N && n != N)
-	{
-		if (m == row && n == col)
-		{
-			m--, n++;
-			continue;
-		}
-		else
-		{
-			if (arr[m][n] == 1)
-			{
-				return 0;
-			}
-			m--, n++;
-		}
->>>>>>> cb147a9b4e81a0102309c628e6bd0e06d8d6bd20
 	}
 	return 1;
 }
-void eightQueen(int row,int col,int (*arr)[N])
+void eightQueen(int row, int col, int(*arr)[N])
 {
-<<<<<<< HEAD
 	int arr2[N][N];
 	for (size_t i = 0; i < N; i++)
 	{
@@ -117,21 +70,12 @@ void eightQueen(int row,int col,int (*arr)[N])
 	if (row == N)
 	{
 		sum++;
-		cout << "第" <<sum <<"种方法"<< endl;
-=======
-	if (row == N)
-	{
-		sum++;
->>>>>>> cb147a9b4e81a0102309c628e6bd0e06d8d6bd20
+		cout << "第" << sum << "种方法" << endl;
 		for (int i = 0; i < N; i++)
 		{
 			for (int j = 0; j < N; j++)
 			{
-<<<<<<< HEAD
-				cout << arr2[i][j]<<" ";
-=======
-				cout << arr[i][j]<<" ";
->>>>>>> cb147a9b4e81a0102309c628e6bd0e06d8d6bd20
+				cout << arr2[i][j] << " ";
 			}
 			cout << endl;
 		}
@@ -145,17 +89,10 @@ void eightQueen(int row,int col,int (*arr)[N])
 			{
 				for (size_t k = 0; k < N; k++)
 				{
-<<<<<<< HEAD
 					arr2[row][k] = 0;
 				}
 				arr2[row][col] = 1;
-				eightQueen(row+1,col,arr2);
-=======
-					arr[row][k] = 0;
-				}
-				arr[row][col] = 1;
-				eightQueen(row+1,col,arr);
->>>>>>> cb147a9b4e81a0102309c628e6bd0e06d8d6bd20
+				eightQueen(row + 1, col, arr2);
 			}
 		}
 	}
@@ -165,7 +102,7 @@ int main()
 {
 	int arr[N][N]{ 0 };
 
-	eightQueen(0,0,arr);
+	eightQueen(0, 0, arr);
 
 	return 0;
 }
